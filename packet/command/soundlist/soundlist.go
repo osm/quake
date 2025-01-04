21e0980c058d068985b8ad78f95fe64d3119485e
Oscar Linderholm <osm@recv.se>
2025-01-04 23:04:34 +0100

    Support QW 2.10 soundlist

--- packet/command/soundlist/soundlist.go
+++ packet/command/soundlist/soundlist.go
@@ -7,23 +7,32 @@ import (
 )
 
 type Command struct {
-	NumSounds byte
-	Sounds    []string
-	Index     byte
+	ProtocolVersion uint32
+	NumSounds       byte
+	Sounds          []string
+	Index           byte
 }
 
 func (cmd *Command) Bytes() []byte {
 	buf := buffer.New()
 
 	buf.PutByte(protocol.SVCSoundList)
-	buf.PutByte(cmd.NumSounds)
 
-	for i := 0; i < len(cmd.Sounds); i++ {
-		buf.PutString(cmd.Sounds[i])
+	if cmd.ProtocolVersion >= 26 {
+		buf.PutByte(cmd.NumSounds)
+
+		for i := 0; i < len(cmd.Sounds); i++ {
+			buf.PutString(cmd.Sounds[i])
+		}
+		buf.PutByte(0x00)
+
+		buf.PutByte(cmd.Index)
+	} else {
+		for i := 0; i < len(cmd.Sounds); i++ {
+			buf.PutString(cmd.Sounds[i])
+		}
+		buf.PutByte(0x00)
 	}
-	buf.PutByte(0x00)
-
-	buf.PutByte(cmd.Index)
 
 	return buf.Bytes()
 }
@@ -32,25 +41,42 @@ func Parse(ctx *context.Context, buf *buffer.Buffer) (*Command, error) {
 	var err error
 	var cmd Command
 
-	if cmd.NumSounds, err = buf.ReadByte(); err != nil {
-		return nil, err
-	}
+	cmd.ProtocolVersion = ctx.GetProtocolVersion()
 
-	for {
-		var sound string
-		if sound, err = buf.GetString(); err != nil {
+	if cmd.ProtocolVersion >= 26 {
+		if cmd.NumSounds, err = buf.ReadByte(); err != nil {
 			return nil, err
 		}
 
-		if sound == "" {
-			break
+		for {
+			var sound string
+			if sound, err = buf.GetString(); err != nil {
+				return nil, err
+			}
+
+			if sound == "" {
+				break
+			}
+
+			cmd.Sounds = append(cmd.Sounds, sound)
 		}
 
-		cmd.Sounds = append(cmd.Sounds, sound)
-	}
+		if cmd.Index, err = buf.ReadByte(); err != nil {
+			return nil, err
+		}
+	} else {
+		for {
+			var sound string
+			if sound, err = buf.GetString(); err != nil {
+				return nil, err
+			}
 
-	if cmd.Index, err = buf.ReadByte(); err != nil {
-		return nil, err
+			if sound == "" {
+				break
+			}
+
+			cmd.Sounds = append(cmd.Sounds, sound)
+		}
 	}
 
 	return &cmd, nil
